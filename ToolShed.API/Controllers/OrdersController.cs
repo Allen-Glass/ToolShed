89b5f1e37d40a0e7393aee32db75444eebda24de
Allen Glass <alglas@microsoft.com>
2019-02-19 21:18:53 -0800

    adding empty controller commands

---
+++ ToolShed.API/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Toolshed.Models.Scheduler;
+
+namespace ToolShed.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> PlaceOrderAsync(Reservation reservation, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("The information submitted is not in the appropriate format");
+            }
+
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ToolShed.API/Controllers/UsersController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToolShed.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUserAsync([FromBody] UserInformation userInformation)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("The user information is incomplete");
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUserDetailsAsync(string userId)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("The user information is incomplete");
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateUserAccountAsync([FromBody] UserInformation userInformation)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("The user information is incomplete");
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpDelete("unregister/{userId}")]
+        public async Task<IActionResult> DeleteUserAsync(string userId)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("The user information is incomplete");
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
