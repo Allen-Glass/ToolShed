9501e50c84952feca9fbecd308b708e063030371
Allen Glass <alglas@microsoft.com>
2019-04-22 22:07:25 -0700

    adding keyvault access

--- ToolShed.Api/Program.cs
+++ ToolShed.Api/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -19,6 +20,23 @@ namespace ToolShed
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                if (!context.HostingEnvironment.IsDevelopment())
+                {
+                    var appConfig = config.Build();
+                    var certStore = new X509Store(StoreLocation.CurrentUser);
+                    certStore.Open(OpenFlags.ReadOnly |
+                                   OpenFlags.OpenExistingOnly);
+                    var cert = certStore.Certificates.Find(X509FindType.FindByThumbprint
+                        , appConfig["CertificateThumbprint"]
+                        , false);
+                    config.AddAzureKeyVault(
+                        appConfig["KeyVaultUrl"]
+                        , appConfig["ClientId"]
+                        , cert.OfType<X509Certificate2>().FirstOrDefault());
+                }
+            })
                 .UseStartup<Startup>();
     }
 }
